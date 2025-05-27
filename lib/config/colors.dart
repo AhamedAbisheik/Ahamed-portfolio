befcd6f29f23cd788a39fa2e0d4156eb910dd7ce
Ahamed777 <abisheik@kumarappa.com>
2025-05-27 15:03:20 +0530

    Updated about section in dev branch

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 mixin AppColors {
   static final yellow = Colors.yellow[800];
-  static final red = Colors.redAccent;
-  static final orange = Colors.orangeAccent;
   static final black = Colors.black87;
   static final blue = Colors.blueAccent;
   static final green = Colors.green;

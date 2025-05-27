239cd4c9342ae0cd4ccb86f217ab09372ff41163
Ahamed777 <abisheik@kumarappa.com>
2025-05-27 13:23:53 +0530

    remove space

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 
 mixin AppColors {
   static final yellow = Colors.yellow[800];
-
   static final black = Colors.black87;
   static final blue = Colors.blueAccent;
   static final green = Colors.green;
   static final white = Colors.white;
-
   static final greyLight = Colors.grey[100];
 }
